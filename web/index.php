471d742aa00e75cc0e7c10221a34b711751bafe7
zdurkin <zdurkin@kickbackpoints.com>
2017-02-24 07:40:05 -0800

    changing seaseme street character

--- web/index.php
+++ web/index.php
@@ -6,11 +6,11 @@
     <body>
 <?php
 
-if(array_key_exists("Red_Guy's_name", $_COOKIE)&&preg_match('/([Ee])(lmo)+/', $_COOKIE["Red_Guy's_name"])){
-  echo('<p>You got it! Flag{omm_nom_nom}</p>');
+if(array_key_exists("Yellow_Guy's_name", $_COOKIE)&&preg_match('/([Bb])(ig)\s([Bb])(ird)+/', $_COOKIE["Yellow_Guy's_name"])){
+  echo('<p>You got it! flag{bigest_of_the_birds}</p>');
 }else{
-    echo("<p>He's my favorite red guy</p>");
-    setcookie("Red_Guy's_name", 'Name', time()+300);
+    echo("<p>He's my favorite Yellow guy</p>");
+    setcookie("Yellow_Guy's_name", 'Name', time()+300);
 }
 
 ?>
