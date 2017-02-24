da452b116913deb67fb6396fe853138654dfd67d
zdurkin <zdurkin@kickbackpoints.com>
2017-02-24 07:29:54 -0800

    adding key

--- web/index.php
+++ web/index.php
@@ -7,8 +7,7 @@
 <?php
 
 if(array_key_exists("Red_Guy's_name", $_COOKIE)&&preg_match('/([Ee])(lmo)+/', $_COOKIE["Red_Guy's_name"])){
-  echo('<p>You got it! Flag: #omm\nom\nom</p>');
-  echo('<p>WORD: Go</p>');
+  echo('<p>You got it! Flag{omm_nom_nom}</p>');
 }else{
     echo("<p>He's my favorite red guy</p>");
     setcookie("Red_Guy's_name", 'Name', time()+300);
