28d86a21668de71b5cde665f8064dca4ac99cf9c
Zane Durkin <zanedurkin@gmail.com>
2017-02-24 20:56:38 -0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,18 +1,20 @@
+<?php
+if(array_key_exists("Yellow_Guy's_name", $_COOKIE)&&preg_match('/([Bb])(ig)\s([Bb])(ird)+/', $_COOKIE["Yellow_Guy's_name"])){
+  $output=('<p>You got it! flag{bigest_of_the_birds}</p>');
+}else{
+    $output = ("<p>He's my favorite Yellow guy</p>");
+    setcookie("Yellow_Guy's_name", 'Name', time()+300);
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Cookie_monster</title>
     </head>
     <body>
-<?php
-
-if(array_key_exists("Yellow_Guy's_name", $_COOKIE)&&preg_match('/([Bb])(ig)\s([Bb])(ird)+/', $_COOKIE["Yellow_Guy's_name"])){
-  echo('<p>You got it! flag{bigest_of_the_birds}</p>');
-}else{
-    echo("<p>He's my favorite Yellow guy</p>");
-    setcookie("Yellow_Guy's_name", 'Name', time()+300);
-}
-
-?>
+    <?php
+    echo($output);    
+    ?>
 </body>
 </html>
